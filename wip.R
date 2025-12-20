e235ee951c203d8ca668c9d969e21c33c4af0df4
Sarah Whitaker <144968123+slwhit04@users.noreply.github.com>
2025-12-19 17:12:41 -0700

    Add files via upload

--- wip.R
+++ wip.R
@@ -7,6 +7,7 @@ library(stringr)
 library(ggplot2)
 library(readxl)
 
+
 ##### read in data
 ANIME <- read_excel("ANIME.xlsx")
 critic_reviews <- read_csv("ghibli/critic_reviews.csv/critic_reviews.csv")
@@ -14,6 +15,7 @@ movies <- read_csv("ghibli/movies.csv/movies.csv")
 studioghibli <- read.csv("C:/Users/19132/Downloads/Studio Ghibli.csv", header=FALSE)
 rotten_tomatoes_movies <- read_csv("ghibli/rotten_tomatoes_movies.csv")
 anime <- read_csv("ghibli/anime.csv")
+anime_dataset <- read_csv("anime_dataset.csv")
 
 
 #### clean studioghibli
@@ -91,6 +93,18 @@ ANIME <- ANIME %>%
   mutate(movieTitle = ifelse(movieTitle == "NAUSICAA OF THE VALLEY OF THE WIND",
                              "NAUSICAÄ OF THE VALLEY OF THE WIND",
                              movieTitle))
+
+
+ANIME <- ANIME %>%
+  mutate(movieTitle = case_when(
+    movieTitle == "THE TALE OF THE PRINCECSS KAGUYA" ~ "THE TALE OF THE PRINCESS KAGUYA",
+    movieTitle == "DEMON SLAYER -KIMESTSU NO YAIBA- THE MOVIE: MUGEN TRAIN" ~ "DEMON SLAYER -KIMETSU NO YAIBA- THE MOVIE: MUGEN TRAIN",
+    movieTitle == "DRAGON BALL SUPER:BROLY" ~ "DRAGON BALL SUPER: BROLY",
+    movieTitle == "ON-GAKU:OUR SOUND" ~ "ON-GAKU: OUR SOUND",
+    movieTitle == "THE CASTLE OF CAGLISTRO" ~ "THE CASTLE OF CAGLIOSTRO",
+    TRUE ~ movieTitle
+  ))
+
 #### joining data my nightmare
 
 
@@ -102,6 +116,84 @@ anime_dataset <- merged %>%
   select(-movieId, -movieURL, )
 
 write_csv(anime_dataset, "anime_dataset.csv")
+
+
+#### EDA
+
+
+ghibli <- anime_dataset %>% filter(STUDIO == "Ghibli")
+non_ghibli <- anime_dataset %>% filter(STUDIO != "Ghibli")
+
+library(ggplot2)
+
+
+# genre
+
+anime_dataset %>%
+  mutate(is_ghibli = if_else(STUDIO == "Ghibli", "Ghibli", "Other")) %>%
+  pivot_longer(
+    cols = starts_with("genre"), 
+    names_to = "genre_rank", 
+    values_to = "genre"
+  ) %>%
+  filter(!is.na(genre) & genre != "") %>%
+  count(is_ghibli, genre, sort = TRUE) %>%
+  ggplot(aes(x = reorder(genre, n), y = n, fill = is_ghibli)) +
+  geom_col(position = "dodge") +
+  coord_flip() +
+  labs(
+    title = "Genre Counts (All Genre Columns): Ghibli vs. Others",
+    x = "Genre",
+    y = "Count"
+  )
+
+
+# theme
+
+anime_dataset %>%
+  mutate(is_ghibli = if_else(STUDIO == "Ghibli", "Ghibli", "Other")) %>%
+  pivot_longer(
+    cols = starts_with("theme"), 
+    names_to = "theme_rank", 
+    values_to = "theme"
+  ) %>%
+  filter(!is.na(theme) & theme != "") %>%
+  count(is_ghibli, theme, sort = TRUE) %>%
+  ggplot(aes(x = reorder(theme, n), y = n, fill = is_ghibli)) +
+  geom_col(position = "dodge") +
+  coord_flip() +
+  labs(
+    title = "Theme Counts (All Theme Columns): Ghibli vs. Others",
+    x = "Theme",
+    y = "Count"
+  )
+
+# setting
+anime_dataset %>%
+  mutate(is_ghibli = if_else(STUDIO == "Ghibli", "Ghibli", "Other")) %>%
+  count(is_ghibli, SETTING, sort = TRUE) %>%
+  ggplot(aes(x = reorder(SETTING, n), y = n, fill = is_ghibli)) +
+  geom_col(position = "dodge") +
+  coord_flip() +
+  labs(title = "Setting Counts: Ghibli vs. Others",
+       x = "Setting",
+       y = "Count")
+
+
+# hair and eye color
+anime_dataset %>%
+  mutate(is_ghibli = if_else(STUDIO == "Ghibli", "Ghibli", "Other")) %>%
+  count(is_ghibli, MC_HAIRCOLOR, MC_EYECOLOR) %>%
+  group_by(is_ghibli) %>%
+  mutate(prop = n / sum(n)) %>%
+  ggplot(aes(x = MC_HAIRCOLOR, y = MC_EYECOLOR, fill = prop)) +
+  geom_tile(color = "white") +
+  facet_wrap(~is_ghibli) +
+  labs(title = "Hair vs. Eye Color Patterns: Ghibli vs. Others", 
+       x = "Hair Color", y = "Eye Color", fill = "Proportion") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
 
 
 
